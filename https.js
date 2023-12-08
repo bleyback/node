ba53b08d8c8b407bdfa249635cb2c489c027d4ce
bleyback <samuelius32@gmail.com>
2023-12-08 16:38:30 -0600

    Creacion de servidor, escuhando en POST y GET

---
+++ https.js
@@ -0,0 +1,24 @@
+const { prototype } = require('node:events')
+const http =require('node:http')
+
+const desirePORT= process.argv.PORT ?? 1234
+
+
+const server = http.createServer((req,res)=>{
+    res.setHeader('Content-Type','text/html;charset=utf-8')
+    if(req.url=='/'){
+        res.statusCode=200
+        res.end('Bienvenído a mi api')
+    }else if(req.url=='/contacto'){
+        res.statusCode=200
+        res.end('<h1>3121691026</h1>')
+    }else{
+        res.statusCode=400
+        res.end('<h1>Error 404</h1>')
+    }
+
+})
+
+server.listen(desirePORT,()=>{
+    console.log(`server listening on port http://localhost:${desirePORT}`)
+})
